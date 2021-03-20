8faffc0e4a8f20b5d2a2117407b0326d62f25e45
rizkyiqbal <rizkiiqbal36@gmail.com>
2021-03-20 14:37:49 +0700

    ci: 🎡 fix copy file

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/cat', router)
 app.get('/', (req, res) => {
-  res.send('your app is running on docker container')
+  res.send('your app is running')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
